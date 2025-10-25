ce9653035f770853561ad7424a939b57cc555436
tzufBarz <81tzufb5@dalet.school>
2025-10-25 21:31:14 +0300

    Transferred notes to search params

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { useParams } from "next/navigation";
 import { NotePage } from "./NotePage";
+import { useSearchParams } from "next/navigation";
 
 export default function NotePageRoute() {
-    const { id } = useParams();
+    const id = useSearchParams().get('id');
     return <NotePage id={Number(id)} />;
 }
